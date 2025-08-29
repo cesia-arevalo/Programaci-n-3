2816d86adae0a1da4a8b6ab926cbfc84a098936f
cesia-arevalo <ustr027024@ugb.edu.sv>
2025-08-29 09:04:15 -0600

    Formulario basico

---
+++ ventana01.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+class Aplicacion: new
+     def __init__(self):
+         formulario01 = tk.TK()
+         formulario01.title('Cesia')
+         formulario01.mainloop()
+
+         llamada = Aplicacion
+
+
+
+
